c5efd990ebd29b37fa6b39bf9304af053b10e5a1
Rafael Gimenez Dotta <rafaelgdotta@gmail.com>
2020-03-30 22:54:53 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,64 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const request = require('request');
+const https = require('https');
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//
+
+app.use(express.static("public"));
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + "/signup.html");
+})
+
+app.post('/', (req, res) => {
+
+    const firstName = req.body.fName;
+    const lastName = req.body.lName;
+    const email = req.body.email;
+
+    const data = {
+        members: [{
+            email_address: email,
+            status: "subscribed",
+            merge_fields: {
+                FNAME: firstName,
+                LNAME: lastName
+            }
+        }]
+    }
+
+    const jsonData = JSON.stringify(data);
+
+    const url = "https://us19.api.mailchimp.com/3.0/lists/c7728996f0";
+
+    const options = {
+        method: "POST",
+        auth: "rafa1:bd14a69fc534067350994727f934a62d-us19"
+    }
+
+    const request = https.request(url, options, (response) => {
+
+        if (response.statusCode === 200) {
+            res.sendFile(__dirname + '/success.html');
+        } else {
+            res.sendFile(__dirname + '/failure.html');
+        }
+
+        response.on('data', (data) => {
+            console.log(JSON.parse(data));
+        })
+    })
+
+    request.write(jsonData);
+    request.end();
+
+})
+
+app.post("/failure", (req, res) => res.redirect("/"));
+
+
+app.listen(3000, () => console.log("Server ok"));
